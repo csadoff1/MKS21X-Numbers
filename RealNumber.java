bc52149af6fb5be28e7081dca60e99cb015e66e0
Camille Sadoff <csadoff00@stuy.edu>
2018-10-15 22:17:12 -0400

    add methods to RealNumber

--- RealNumber.java
+++ RealNumber.java
@@ -1,3 +1,13 @@
 public class RealNumber{
-  private double x;
+  private double x = 10;
+
+  public double getX() {
+    return x;
+  }
+  public void setX(double X) {
+    x = X;
+  }
+  public String toString() {
+    return "Real number: " + x;
+  }
 }
