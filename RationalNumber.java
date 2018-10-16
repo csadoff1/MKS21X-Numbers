fa220de000a6dd5c85138ddd3cd38f069a63bf5b
Camille Sadoff <csadoff00@stuy.edu>
2018-10-15 22:10:34 -0400

    add methods

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,22 @@
 public class RationalNumber extends RealNumber{
   private int x, y;
+
+  public double toRatio() {
+    return x/y;
+  }
+  public int getX() {
+    return x;
+  }
+  public int getY() {
+    return y;
+  }
+  public void setX(int X) {
+    x=X;
+  }
+  public void setY(int Y) {
+    y=Y;
+  }
+  public String toString() {
+    return getX() + "//" + getY();
+  }
 }
