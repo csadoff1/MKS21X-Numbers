f0a87d2350795cfcf6d630ffbc98fc16a5887f0c
csadoff1 <43050255+csadoff1@users.noreply.github.com>
2018-10-16 21:47:48 -0400

    Updated manually because git push deleted edits

--- RationalNumber.java
+++ RationalNumber.java
@@ -18,9 +18,26 @@ public class RationalNumber extends RealNumber{
   public String toString() {
     return getNumerator() + "//" + getDenominator();
   }
-  public RationalNumber reciprocal(int nume, int deno) {
-    nume = deno;
-    deno = nume;
+  public RationalNumber reciprocal(int num, int den) {
+    RationalNumber recip = new RationalNumber(den, num);
+    return recip;
   }
-  public boolean equals()
+  public boolean equals(RationalNumber other) {
+    return (this.getNumerator() == other.getNumerator()
+            && this.getDenominator() == other.getDenominator());
+  }
+  public static int gcd(int a, int b) {
+    if (a<b) {
+      a=b;
+      b=a;
+    }
+      int r = a % b;
+      if (r == 0) {
+        return b;
+      }
+      else {
+        a=b;
+        b=r;
+      }
+  }
 }
